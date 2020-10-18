337872119f46b37f4381b7cc52eb9e625825a833
MistyEye2088 <1956624626@qq.com>
2020-10-18 22:57:34 +0800

    Add files via upload

---
+++ matrixCal/cmake-build-debug/CMakeFiles/matrixCal.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/matrixCal.dir/main.cpp.obj"
+  "libmatrixCal.dll.a"
+  "matrixCal.exe"
+  "matrixCal.exe.manifest"
+  "matrixCal.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/matrixCal.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
